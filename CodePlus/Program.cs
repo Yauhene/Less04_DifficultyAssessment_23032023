60590f4ed72af918aee7bfdeee99cfefbd8d1ef1
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-03-24 21:54:05 +0300

    add listing for CodePlus

--- CodePlus/Program.cs
+++ CodePlus/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int n = Random.Shared.Next(100000);
+double s = 0;
+for (int j = 2; j < n / 2; j++)
+{
+    int count = 0;
+    int i = n;
+    while (i > 0)
+    {
+        count++;
+        //s += i;
+        i = i / 2;
+    }
+    Console.WriteLine($"Для числа n: {n}  количество операций count: {count}");
+}
