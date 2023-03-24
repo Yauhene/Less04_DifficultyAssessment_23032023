a0b8e342b0eab121eb123a345817b3bd3828e328
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-03-24 20:07:46 +0300

    add Code folder

---
+++ Code/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+double s = 0;
+for (int n = 0; n < 15; n++)
+{
+    int count = 0;
+    int i = n;
+    while (i > 0)
+    {
+        count++;
+        s += i;
+        i = i / 2;
+    }
+    Console.WriteLine($"n: {n}  count: {count}");
+}
